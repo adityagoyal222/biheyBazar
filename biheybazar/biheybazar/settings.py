a8f0252a65239958efffc48df56a01d3cb17dacd
Aditya Goyal <goyaladitya85@gmail.com>
2020-11-26 13:21:58 +0545

    database setup

--- biheybazar/biheybazar/settings.py
+++ biheybazar/biheybazar/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'biheybazar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'biheybazar',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
