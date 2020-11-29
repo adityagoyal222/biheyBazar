3bc7f463b6e8cc895c7d52bb3a2b3c2585c10f0e
Aditya Goyal <goyaladitya85@gmail.com>
2020-11-29 15:43:44 +0545

    created model for User and Customer

--- biheybazar/customers/models.py
+++ biheybazar/customers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Student(models.Model):
+    LOCATION_CHOICES = (
+        (1, "Kathmandu"),
+        (2, "Pokhara"),
+        (3, "Chitwan"),
+        (4, "Janakpur"),
+        (5, "Dang")
+    )
+    CULTURE_CHOICES = (
+        (1, 'Brahmin'),
+        (2, 'Newar'),
+        (3, 'Tharu'),
+        (4, 'Terai')
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    location = models.PositiveIntegerField(choices=LOCATION_CHOICES)
+    culture = models.PositiveIntegerField(choices=CULTURE_CHOICES)
--- biheybazar/users/models.py
+++ biheybazar/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+# Creates an AbstractUser called User for authentication 
+class User(AbstractUser):
+    is_customer = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
