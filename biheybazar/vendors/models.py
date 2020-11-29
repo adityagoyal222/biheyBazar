0efe8620f423ef19f942e9870f4a8f73311b47da
Aditya Goyal <goyaladitya85@gmail.com>
2020-11-29 16:17:09 +0545

    created vendor model. authentication models complete

--- biheybazar/vendors/models.py
+++ biheybazar/vendors/models.py
@@ -3,13 +3,12 @@ from django.contrib.auth.models import User
 from django.db.models.deletion import CASCADE
 
 #Create your models here.
-# class Vendor(models.Model):
+class Vendor(models.Model):
 
-#     vendor_name = models.CharField(max_length=200, blank=False)
-#     logo = models.ImageField(upload_to='logo')
-#     cover_image = models.ImageField(upload_to='cover_image')
-
-
+    vendor_name = models.CharField(max_length=200, blank=False)
+    logo = models.ImageField(upload_to='logo')
+    cover_image = models.ImageField(upload_to='cover_image')
+    about = models.TextField()
 
 class VendorImage(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=CASCADE, related_name='images')
