81bbd2db18d57e56aa414f2c22621187ad1584d7
Aditya Goyal <goyaladitya85@gmail.com>
2020-11-30 20:09:14 +0545

    updated gitignore and requirements.txt file

--- biheybazar/biheybazar/settings.py
+++ biheybazar/biheybazar/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media/')
 
 
 # Quick-start development settings - unsuitable for production
