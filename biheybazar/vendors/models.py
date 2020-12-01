9689587b2d2531459845c8eed129d523401599c9
Aditya Goyal <goyaladitya85@gmail.com>
2020-12-01 14:06:11 +0545

    added Categories model

--- biheybazar/vendors/models.py
+++ biheybazar/vendors/models.py
@@ -7,12 +7,20 @@ from vendors.models import Vendor
 from django.db.models.deletion import CASCADE
 
 #Create your models here.
+class Categories(models.Model):
+    tag_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.tag_name
+
 class Vendor(models.Model):
     user = models.OneToOneField(User, on_delete=CASCADE, primary_key=True)
     vendor_name = models.CharField(max_length=200, blank=False)
     logo = models.ImageField(upload_to='logo')
     cover_image = models.ImageField(upload_to='cover_image')
     about = models.TextField()
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name='category')
 
     # String representation of vendor model
     def __str__(self):
@@ -47,7 +55,3 @@ class VendorTag(models.Model):
 
     class Meta:
         unique_together = ('vendor', 'tag')
-
-
-class Categories(models.Model):
-    pass
