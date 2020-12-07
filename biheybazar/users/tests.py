289012a920981b0e240b7470f0264579f00908cf
Aditya Goyal <goyaladitya85@gmail.com>
2020-12-07 22:14:30 +0545

    added a few tests

--- biheybazar/users/tests.py
+++ biheybazar/users/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 import pytest
 from .models import User
+from customers.models import Customer
+from vendors.models import Vendor
 from django.urls import reverse
 
 # Create your tests here.
@@ -8,4 +10,3 @@ from django.urls import reverse
 def test_user_create():
     user = User.objects.create_user('john', 'lennon@thebeatles.com', 'softwarica')
     assert User.objects.count() == 1
-
