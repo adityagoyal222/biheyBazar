c81d387f5db5da9071fe241be16fa411c8fd7f9c
gbibek34 <gbibek34@gmail.com>
2021-01-05 18:14:19 +0545

    vendor profile, vendor signin, has bugs

---
+++ biheybazar/static/js/vendor.js
@@ -0,0 +1,14 @@
+$(".info-tabs ul li").click(function () {
+  $(this).addClass("active").siblings().removeClass("active");
+});
+
+const tabBtn = document.querySelectorAll(".info-tabs ul li");
+const tab = document.querySelectorAll(".tab");
+
+function tabs(panelIndex) {
+  tab.forEach(function (node) {
+    node.style.display = "none";
+  });
+  tab[panelIndex].style.display = "block";
+}
+tabs(0);
