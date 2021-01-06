df8309b042d807e5cea67c317fb3d0dd74e4c334
Aditya Goyal <goyaladitya85@gmail.com>
2021-01-06 19:39:56 +0545

    solved vendor signup

--- biheybazar/biheybazar/settings.py
+++ biheybazar/biheybazar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'yim8q#2mz*hgx&c5bc9b+_ypi1qa_j=z*@oaqw(4yu!(rt3le1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['biheybazar.pythonanywhere.com']
+ALLOWED_HOSTS = ['biheybazar.pythonanywhere.com', '127.0.0.1']
 
 INTERNAL_IPS = [
     # ...
