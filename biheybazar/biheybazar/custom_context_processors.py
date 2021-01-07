ad1a5890f50944d1bac11ffe5cd593899d9d6da7
Aditya Goyal <goyaladitya85@gmail.com>
2021-01-07 11:20:19 +0545

    fixed vendor_list

--- biheybazar/biheybazar/custom_context_processors.py
+++ biheybazar/biheybazar/custom_context_processors.py
@@ -2,7 +2,14 @@ from customers.models import Customer
 from vendors.models import Vendor
 
 def customer_vendor(request):
+    customer = ''
+    vendor = ''
+    if request.user.is_authenticated:
+        if request.user.is_customer:
+            customer = Customer.objects.filter(user = request.user).first()
+        elif request.user.is_authenticated:
+            vendor = Vendor.objects.filter(user = request.user).first()
     return {
-        "customer_context": Customer.objects.filter(user = request.user).first(),
-        "vendor_context": Vendor.objects.filter(user = request.user).first(),
+        "customer_context": customer,
+        "vendor_context": vendor,
     }
